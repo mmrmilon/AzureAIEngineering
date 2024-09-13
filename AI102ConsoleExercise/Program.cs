aff396237be36b844d281354817b69656adf633e
Mizanur Rahman <mr.milonku@hotmail.com>
2024-09-13 22:26:44 +0800

    Detect the language of a single and multiple document

--- AI102ConsoleExercise/Program.cs
+++ AI102ConsoleExercise/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace AI102ConsoleExercise
+﻿using System;
+using Azure;
+using Azure.AI.TextAnalytics;
+
+namespace AI102ConsoleExercise
 {
     internal class Program
     {
+        private static readonly AzureKeyCredential credential = new("<apiKey>");
+        private static readonly Uri endpoint = new("<endpoint>");
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new TextAnalyticsClient(endpoint, credential);
+
+            MultiLanguageDetection(client);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void SingleLanguageDetection(TextAnalyticsClient client)
+        {
+            var document = "Este documento está escrito en un lenguaje diferente al inglés. Su objectivo es demostrar cómo" 
+                + "The hotel has some great staff but the location is not in a good neighborhood.";
+
+            var response = client.DetectLanguage(document);
+
+            Console.WriteLine($"Language: {response.Value.Name}, \tISO-6391: {response.Value.Iso6391Name}");        
+        }
+
+        static void MultiLanguageDetection(TextAnalyticsClient client)
+        {
+            var documents = new List<string>
+            {
+                @"Este documento está escrito en un lenguaje diferente al inglés.",
+                @"The hotel has some great staff but the location is not in a good neighborhood.",
+                @"Selamat pagi. Mau makan untuk sarapan?",
+                @"Hola, buenos dias. Me gustaria una torlilla de patata y dos cafes con leche."
+            };
+
+            foreach (var document in documents) 
+            {
+                var response = client.DetectLanguage(document);
+
+                Console.WriteLine($"Language: {response.Value.Name}, \tISO-6391: {response.Value.Iso6391Name}");
+            }
         }
     }
 }
